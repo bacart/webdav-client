cca493ceca7310c3982dee799f9b1d987a400cad
Alex Bacart <13940752+alex-bacart@users.noreply.github.com>
2018-12-01 12:34:01 +0300

    CS fixes

--- src/Client/AbstractWebdavClient.php
+++ src/Client/AbstractWebdavClient.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Bacart package.
+ *
+ * (c) Alex Bacart <alex@bacart.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Bacart\WebdavClient\Client;
 
 use Bacart\Common\Exception\MissingPackageException;
@@ -250,6 +259,8 @@ abstract class AbstractWebdavClient implements WebdavClientInterface
      * @param string $size
      * @param string $created
      * @param string $modified
+     *
+     * @throws \Exception
      *
      * @return WebdavDtoInterface
      */
